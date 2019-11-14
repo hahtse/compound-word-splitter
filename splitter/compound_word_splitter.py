0dfc75634888805d08262809bbdfd9c4a2b552f2
Hans-Christian Heinz <h.heinz@cid.com>
2019-11-14 10:10:06 +0100

    changed minimum target chunk size to 3 to avoid non-noun words that frequently get confused with endings ('er')

--- splitter/compound_word_splitter.py
+++ splitter/compound_word_splitter.py
@@ -18,7 +18,7 @@ def __capitalize_first_char(word):
     return word[0].upper() + word[1:]
 
 
-def split(word, language='en_us',ignore_word=''):
+def __split(word, language='en_us',ignore_word=''):
     dictionary = enchant.Dict(language)
     max_index = len(word)
     for index, char in enumerate(word):
@@ -29,19 +29,22 @@ def split(word, language='en_us',ignore_word=''):
             right_compound1_upper = right_compound_1[0].isupper()
         if right_compound_2:
             right_compound2_upper = right_compound_2[0].isupper()
-        if index > 0 and len(left_compound) > 1 and not dictionary.check(left_compound):
+        if index > 0 and len(left_compound) > 2 and not dictionary.check(left_compound):
             left_compound = __capitalize_first_char(left_compound)
-        is_left_compound_valid_word = len(left_compound) > 1 and dictionary.check(left_compound)
-        if is_left_compound_valid_word and left_compound != ignore_word and ((not split(right_compound_1, language) == '' and not right_compound1_upper) or right_compound_1 == ''):
-            splitword = split(left_compound,language,left_compound)
-            returnvar = [compound for compound in __concat(__concat(left_compound, split(right_compound_1, language)),splitword) if not compound == '']
+        is_left_compound_valid_word = len(left_compound) > 2 and dictionary.check(left_compound)
+        if is_left_compound_valid_word and left_compound != ignore_word and ((not __split(right_compound_1, language) == '' and not right_compound1_upper) or right_compound_1 == ''):
+            splitword = __split(left_compound,language,left_compound)
+            returnvar = [compound for compound in __concat(__concat(left_compound, __split(right_compound_1, language)),splitword) if not compound == '']
             return returnvar
-        elif is_left_compound_valid_word and word[max_index-index:max_index-index+1] == 's' and ((not split(right_compound_2, language) == '' and not right_compound2_upper) or right_compound_2 == ''):
-            return [compound for compound in __concat(left_compound, split(right_compound_2, language))\
+        elif is_left_compound_valid_word and word[max_index-index:max_index-index+1] == 's' and ((not __split(right_compound_2, language) == '' and not right_compound2_upper) or right_compound_2 == ''):
+            return [compound for compound in __concat(left_compound, __split(right_compound_2, language))\
                     if not compound == '']
-    if not word == '' and len(word)>1 and dictionary.check(word):
+    if not word == '' and len(word)>2 and dictionary.check(word):
          return [word]
-    elif not word == '' and len(word)>1 and dictionary.check(__capitalize_first_char(word)):
+    elif not word == '' and len(word)>2 and dictionary.check(__capitalize_first_char(word)):
         return [__capitalize_first_char(word)]
     else:
         return ''
+        
+def split(word, language='en_us'): 
+    return list(dict.fromkeys(__split(word,language)))
